609d5ba09dbbfd0be7d62991abef0a56ba24d9f7
Archil Gogia <arshileg@gmail.com>
2022-08-10 15:19:47 +0400

    Created mocks and type for films

---
+++ project/src/types/films.ts
@@ -0,0 +1,10 @@
+export type Films = {
+    id: number,
+    title: string,
+    genre: string,
+    year: string,
+    director: string
+    actors: string[],
+    poster: string,
+    src: string,
+};
